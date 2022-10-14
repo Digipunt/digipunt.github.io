ebca4e3be3fe859654c0f398985ffbb2d7ae95c4
Ben <35698543+BenjamienSD@users.noreply.github.com>
2022-10-14 08:56:12 +0200

    Update scripts/workshopList.js

--- scripts/workshopList.js
+++ scripts/workshopList.js
@@ -3,30 +3,6 @@ console.log('workshopList loaded');
 const workshopList = [
   // WORKSHOP 1
   {
-    value: 'word',
-    titel: 'Microsoft Word voor beginners',
-    datum: '11, 12 & 13 oktober',
-    tijd: '14u tot 16u',
-    locatie: 'Vlaamsekaai 10',
-    inschrijving: 'https://forms.office.com/r/6CnCb7zk0R',
-    info: {
-      beschrijving:
-        'Wil je graag kennismaken met Word of leren werken met een tekstverwerkingsprogramma? Dan is de lessenreeks Word (Office) iets voor jou.',
-      onderwerpen: [
-        'het lint leren kennen',
-        'een document opmaken',
-        'een tabel & afbeelding invoegen en aanpassen',
-        'sjablonen gebruiken',
-        'een flyer maken',
-      ],
-      extra:
-        'In de les gebruik ik Word Online dat gratis toegankelijk is via een Microsoft account. Dit is een beperktere versie maar geschikt voor basisgebruik. Gebruik je een ander tekstverwerkingsprogramma, dan kun je ook deelnemen. Hou er wel rekening mee dat iconen, omschrijvingen en opties kunnen verschillen. Deze lessenreeks is geschikt voor mensen met weinig tot geen kennis van Word.',
-    },
-    status: 'volzet of # plaatsen beschikbaar',
-  },
-
-  // WORKSHOP 2
-  {
     value: 'android',
     titel: '  Basis Android',
     datum: '18, 19 & 20 oktober',

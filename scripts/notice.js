04fffe303f344362b1c4808101423377261fb3a4
BenjamienSD <benjamien.deprez.dtp@gmail.com>
2023-01-02 09:08:59 +0100

    yolo

--- scripts/notice.js
+++ scripts/notice.js
@@ -1,7 +1,6 @@
-let state = true;
+let state = false;
 
 let boodschap = {
-  titel: 'Aangepaste openingsuren',
-  inhoud:
-    'Vrijdag 23/12 is het Digipunt Vlaamsekaai in de namiddag gesloten. Vrijdag 30/12 is het Digipunt Vlaamsekaai de hele dag gesloten. Dank voor uw begrip.',
+  titel: 'Aankondigingen',
+  inhoud: '',
 };
